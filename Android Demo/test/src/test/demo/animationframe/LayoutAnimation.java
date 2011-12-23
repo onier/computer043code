96fbcb6df9246ce1715582e3a7f87c5e5dce854b
daay1986 <daay1986@d9874f48-9d07-11de-8fab-25195aebac49>
2011-12-23 02:18:49 +0000

    test

---
+++ Android Demo/test/src/test/demo/animationframe/LayoutAnimation.java
@@ -0,0 +1,36 @@
+package test.demo.animationframe;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.TranslateAnimation;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class LayoutAnimation extends ListActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrings));
+
+		AnimationSet set = new AnimationSet(true);
+
+		Animation animation = new AlphaAnimation(0.0f, 1.0f);
+		animation.setDuration(1000);
+		set.addAnimation(animation);
+
+		animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, -1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+		animation.setDuration(1000);
+		set.addAnimation(animation);
+
+		LayoutAnimationController controller = new LayoutAnimationController(set, 0.5f);
+		ListView listView = getListView();
+		listView.setLayoutAnimation(controller);
+	}
+
+	private String[] mStrings = { "Bordeaux", "Lyon", "Marseille", "Nancy", "Paris", "Toulouse", "Strasbourg" };
+}
